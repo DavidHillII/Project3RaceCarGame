45ddc1a7d79207530f95378255a4b601a6bbc2d9
Gabriel Luciano <40375468+gluciano05@users.noreply.github.com>
2025-03-13 12:23:19 -0400

    Worked on movement logic and added race logic class

--- Car.java
+++ Car.java
@@ -1,13 +1,26 @@
 //Class for car
+import java.lang.Math;
+
 public class Car {
     private  Engine engine;
     private Tire tire;
     private Position carPos;
+    private Position targetPos;
     private double speed;
 
     public Car (Engine engine, Tire tire) {
         this.engine = engine;
         this.tire = tire;
         this.speed = this.engine.getSpeedValue() + this.tire.getSpeedValue();
+    }
+
+    public Position getCarPos() {
+        return carPos;
+    }
+
+    public void move() {
+        int deltaX = this.targetPos.getX() - this.carPos.getX();
+        int deltaY = this.targetPos.getY() - this.carPos.getY();
+
     }
 }
