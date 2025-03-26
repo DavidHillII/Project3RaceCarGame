cc7dde721f598b1311bf6538c419eca8a6b92906
Gabriel Luciano <40375468+gluciano05@users.noreply.github.com>
2025-03-26 14:46:55 -0400

    Fixed Error, track wasn't initialized

--- RaceGUI.java
+++ RaceGUI.java
@@ -38,6 +38,7 @@ public class RaceGUI {
      */
     public RaceGUI() {
         logicHandler = new RaceLogic();
+        track = new Track("Race Track 1");
         frame = new JFrame("Car Racing Game");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
